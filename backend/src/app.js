f1644f774a2e27cff418c0ec1164a202135512b7
Taiel Sagretti <taiels94@gmail.com>
2024-04-03 21:16:03 -0300

    Estructura basica del proyecto, coneccion a la DB e instalacion de dotenv

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import  dbConnecion  from './database/database.js';
 
+dotenv.config();
 const app = express();
+dbConnecion()
 
 app.use(express.json());
 
-app.listen(3000);
-console.log('Server on port', 3000);
-
-export default app;
-
-
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
---
+++ backend/src/database/database.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+async function dbConnecion () {
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECT);
+
+        console.log("base de datos online");
+    } catch (error) {
+        console.log(error);
+        throw new Error('Conexion a la base de datos fallida');
+    }
+};
+
+export default dbConnecion
