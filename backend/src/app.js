06d38f7f1cec013c63e8d2c43ba8aab2fc92a86b
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2024-04-06 15:58:05 -0300

    Actualizando backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,8 @@ dbConnecion()
 
 app.use(express.json());
 
-app.use('/login', loginRoute)
-app.use('/signup', signupRoute)
+app.use('/api/login', loginRoute)
+app.use('/api/signup', signupRoute)
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
