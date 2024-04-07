87990ca3f3748913776bfed74f0562aa4bf41578
Martin Casares <casaresrm@gmail.com>
2024-04-07 08:18:12 -0300

    modelo servicios, crud y validaciones

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import  dbConnecion  from './database/database.js';
+import dbConnecion from './database/database.js';
 
-import loginRoute from './routes/login.route.js'
-import signupRoute from './routes/signup.route.js'
+import loginRoute from './routes/login.route.js';
+import signupRoute from './routes/signup.route.js';
+import serviceRoutes from './routes/services.route.js';
 
 dotenv.config();
 const app = express();
-dbConnecion()
+dbConnecion();
 
 app.use(express.json());
 
-app.use('/api/login', loginRoute)
-app.use('/api/signup', signupRoute)
+app.use('/api/login', loginRoute);
+app.use('/api/signup', signupRoute);
+app.use('/services', serviceRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+	console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
