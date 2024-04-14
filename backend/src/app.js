802c15a6d56ff79e883154f6ff55efedad0cb2fc
Taiel Sagretti <taiels94@gmail.com>
2024-04-14 16:55:26 -0300

    agregado codigo faltante

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,83 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import  dbConnecion  from './database/database.js';
+import dbConnection from './database/database.js';
+import { Server } from 'socket.io';
+import exphbs from 'express-handlebars';
+import path from 'path';
+
+// Internal imports
+import chatManager from './controllers/chat.controller.js';
+import userModel from './models/user.model.js';
+import __dirname from './utils.js';
 import loginRoute from './routes/login.route.js'
 import usersRoute from './routes/users.route.js'
 import serviceRoutes from './routes/services.route.js';
+import chatRouter from './routes/chat.route.js'
+
 dotenv.config();
 const app = express();
-dbConnecion()
+dbConnection();
 
 app.use(express.json());
-
 app.use('/api/login', loginRoute);
 app.use('/api/users', usersRoute);
 app.use('/services', serviceRoutes);
+app.use('/chat', chatRouter)
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log(`Server is running on port ${process.env.PORT || 3000}`);
+// Handlebars configuration
+app.engine('hbs', exphbs.engine());
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const httpServer = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
+
+// WebSocket
+const socketServer = new Server(httpServer);
+const users = {};
+
+socketServer.on('connect', async socket => {
+  console.log('New client connected');
+  app.use((req, res, next) => {
+    console.log('APP.JS', req);
+    next()
+  })
+  socket.on('newUser', (arr) => {
+    users[socket.id] = arr.username;
+    socketServer.emit('userConnected', arr.username, arr.proffessionalName);
+    console.log(arr);
+
+    chatManager.returnChat().then(messages => {
+        messages.forEach(message => {
+            socket.emit('message', { username: message.user, message: message.message });
+        });
+    }).catch(error => {
+        console.error(error);
+    });
+});
+
+
+  socket.on('chatMessage', async (arr) => {
+    const username = users[socket.id];
+    if (arr.message.length < 1) {
+      socketServer.emit('error');
+    } else {
+      console.log("hola",arr.proffessionalName);
+      const userio = await userModel.findOne({firstName: users[socket.id]})
+      const pro = await userModel.findOne({firstName: arr.proffessionalName})
+
+      // console.log("dede app: ", arr.message);
+      socketServer.emit('response', chatManager.updateDb(userio, arr.message, pro));
+      const mes = arr.message
+      socketServer.emit('message', { username, mes });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    const username = users[socket.id];
+    delete users[socket.id];
+    socketServer.emit('userDisconnected', username);
+  });
 });
