fc160db43da580efc26078bc143d7aa34557ecf3
F4lleau <ivonnefalleau@gmail.com>
2024-04-15 17:53:51 -0300

    models

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
   firstName: {type: String, required: [true, 'Missing field: first name']},
   lastName: {type: String, required: [true, 'Missing field: lastname']},
   email: {type: String, unique: [true, 'Email already in use.'], required: [true, 'Missing field: email']},
-  age: {type: Number, required: [true, 'Missing field: age']},
   password: {type: String, required: [true, 'Missing field: password']},
   rol: {type: String, required: true, emun: ['admin', 'client', 'provider']},
 });
