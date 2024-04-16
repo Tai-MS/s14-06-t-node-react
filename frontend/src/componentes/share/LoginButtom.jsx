5ee0f6a721db86c53aa3173e3bf82035ae743741
Yein E <yeinneg@gmail.com>
2024-04-16 20:59:57 -0300

    seacrhCard

--- frontend/src/componentes/share/LoginButtom.jsx
+++ frontend/src/componentes/share/LoginButtom.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const LoginButton = ({ to, onSubmit, children }) => {
+export const LoginButton = ({ to, onSubmit, children, width }) => {
+  // Determina el ancho del botón basado en la prop 'width' o establece el valor predeterminado como 'w-[75%]'
+  const buttonWidth = width ? width : 'w-[75%]';
+
   return to ? (
     // Si se proporciona 'to', renderiza como un Link
     <Link
       to={to}
-      className="bg-customGreen hover:bg-green-600 text-black font-semibold w-[75%] py-3 px-4 shadow-md transition-colors duration-300 ease-in-out flex items-center justify-center rounded-full"
+      className={`bg-customGreen hover:bg-green-600 text-black font-semibold ${buttonWidth} py-3 px-4 shadow-md transition-colors duration-300 ease-in-out flex items-center justify-center rounded-full`}
     >
-     {children}
+      {children}
     </Link>
   ) : onSubmit ? (
     // Si se proporciona 'onSubmit', renderiza como un botón de submit
     <button
       type="submit"
-      className="bg-customGreen hover:bg-green-600 text-black font-semibold w-[75%] py-3 px-4 shadow-md transition-colors duration-300 ease-in-out flex items-center justify-center rounded-full"
+      className={`bg-customGreen hover:bg-green-600 text-black font-semibold ${buttonWidth} py-3 px-4 shadow-md transition-colors duration-300 ease-in-out flex items-center justify-center rounded-full`}
       onClick={onSubmit}
     >
       {children}
