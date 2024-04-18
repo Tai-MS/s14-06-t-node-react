d820caa11dbd75417c361d5615d9169ba5033f75
Mary Laura Carolina Limay Oliva <limayolivamary@gmail.com>
2024-04-18 12:34:47 -0500

    areglando rutas

--- frontend/src/route/AuthRouter.jsx
+++ frontend/src/route/AuthRouter.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { HomeClient } from "../componentes/homeUser/HomeClient.jsx";
 import { SearchPage } from "../pages/searchService/SearchPage.jsx";
-
+import { NotFound } from "../pages/NotFound.jsx";
 export default function AuthRouter() {
 
   
@@ -10,6 +10,7 @@ export default function AuthRouter() {
       {/* HOME CLIENT */}
       <Route path="/homeclient" element={<HomeClient />} />  
       <Route path='/resultados-servicio' element={<SearchPage />} />
+      <Route path="/*" element={<NotFound />} /> 
     </Routes>
   );
 }
