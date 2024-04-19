6ded1edd873d64b5cbc0b2c0f7eceb26a3b2ab1f
F4lleau <ivonnefalleau@gmail.com>
2024-04-19 11:53:33 -0300

    estilos registro

--- frontend/src/componentes/Registration/Register.jsx
+++ frontend/src/componentes/Registration/Register.jsx
@@ -25,43 +25,72 @@ const FormRegister = () => {
   });
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
+    <div class="w-full max-w-xs">
+       <div>
+        <form class="w-328 h-568 bg-gradient-to-b from-green-200 to-neutral-400 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}> 
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="firstName">
+        Nombre
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type='text'
           placeholder='Nombre'
           name='firstName'
           value={values.firstName}
           onChange={handleChange}
-        />
-        <input
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="lastName">
+        Apellido
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type='text'
           placeholder='Apellido'
           name='lastName'
           value={values.lastName}
           onChange={handleChange}
-        />
-        <input
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+        Email
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type='email'
           placeholder='Correo electrónico'
           name='email'
           value={values.email}
           onChange={handleChange}
-        />
-        <input
+          />
+        </div>
+        <div class="mb-4">
+          <label  class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Contraseña
+          </label>
+          <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           type='password'
           placeholder='Contraseña'
           name='password'
           value={values.password}
           onChange={handleChange}
-        />
-        <select name='rol' value={values.rol} onChange={handleChange}>
+          />
+        </div>
+        <div class="inline-block relative w-64">
+        <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" name='rol' value={values.rol} onChange={handleChange}>
           <option value='CLIENT'>Cliente</option>
           <option value='PROVIDER'>Proveedor</option>
-        </select>
-        <button type='submit'>Registrarse</button>
-      </form>
+        </select> 
+        </div>
+        
+        <div class="p-4 flex justify-between shadow-md sticky top-0 z-50">
+         <div class="bg-green-200 hover:bg-green-600 rounded-full text-black font-semibold w-max py-3 px-4 shadow-md transition-colors duration-300 ease-in-out flex items-center justify-center">
+          <button type='submit'>Registrarse</button>
+         </div>
+        </div>
+      </form> 
     </div>
+   </div>     
   );
 };
 
