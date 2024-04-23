b1141dc9ac951565d91fbc4121ce7ca653ac84d3
Yein E <yeinneg@gmail.com>
2024-04-23 14:34:33 -0300

    historialAxio

--- frontend/src/componentes/ClientHistorial/ClientHistorialCard.jsx
+++ frontend/src/componentes/ClientHistorial/ClientHistorialCard.jsx
@@ -1,10 +1,13 @@
-import { LoginButton } from "../share/LoginButtom";
+
+import React from 'react';
+import { LoginButton } from '../share/LoginButtom';
 
 export const ClientHistorialCard = ({ proveedoresContratado }) => {
-  //const { firstName, lastName, category, address, phone } = proveedoresContratado;
+  const { category, createdAt, title } = proveedoresContratado;
+console.log(proveedoresContratado)
   return (
-    <div className="py-2 px-2 ">
-      <div className="w-[328px] h-58 bg-[#FAFFFB] rounded-xl shadow-lg border flex flex-col">
+    <div className="py-2 px-2">
+      <div className="w-[324px] h-58 bg-[#FAFFFB] rounded-xl shadow-lg border flex flex-col">
         <div className="flex items-center mb-1 mt-4">
           <img
             className="w-12 h-12 rounded-full ml-4"
@@ -12,7 +15,7 @@ export const ClientHistorialCard = ({ proveedoresContratado }) => {
             alt="User Photo"
           />
           <div className="ml-3">
-            <span className="font-bold">Laura Gonzalez</span>
+            <span className="font-bold">{category}</span>
           </div>
         </div>
         <div className="flex flex-row mt-2">
@@ -27,6 +30,7 @@ export const ClientHistorialCard = ({ proveedoresContratado }) => {
           <label className="font-semibold ml-5">Tarifa por hora: </label>
           <span id="barrioNombre"> $3.000</span>
         </div>
+
         <div className="w-[100%] flex justify-end mt-3 mb-2 p-2">
           <LoginButton to="*" width="w-45%">
             Cancelar Pedido
--- frontend/src/pages/ClientHistory/ClientHistory.jsx
+++ frontend/src/pages/ClientHistory/ClientHistory.jsx
@@ -1,37 +1,51 @@
-import { SearchCard } from "../../componentes/search/SearchCard";
+import React, { useState, useEffect } from "react";
 import { HeaderClient } from "../../componentes/share/HeaderClient";
 import { FooterContact } from "../../componentes/share/FooterContact";
 import { useAuthStore } from "../../hooks/useAuthStore.js";
 import { ClientHistorialCard } from "../../componentes/ClientHistorial/ClientHistorialCard.jsx";
-//import { useLocation } from 'react-router-dom';
+import { fetchClientHistorial } from "../../servicios/ClientHistorialAxio.js";
 
 export const ClientHistorial = () => {
-    const { user, handleLogout }=useAuthStore() 
-  //   const location = useLocation();
-  //   const servicios = location.state?.servicio; // servicios es un array de objetos
+  const { user } = useAuthStore();
+  const [historialData, setHistorialData] = useState([]);
 
-  //   console.log('Los servicios recibidos son:', servicios);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchClientHistorial(user._id);
+        setHistorialData(data);
+      } catch (error) {
+        console.error("Error al obtener el historial:", error);
+      }
+    };
+
+    fetchData();
+  }, [user._id]);
+
+  console.log("Historial Data:", historialData.adquired_services);
 
   return (
     <>
       <HeaderClient showMenu={true} />
 
       <div className="p-4 flex justify-center items-center ">
-      <div className="w-[350px] h-[500px] rounded-xl shadow bg-gradient-to-b from-[#BCF5CC] to-[#86B282]">
-        <h2 className="font-semibold text-2xl mb-3 mt-1 text-center"> {`¡Hola ${user.firstName}!`}</h2>
+        <div className="w-[350px] h-full rounded-xl shadow bg-gradient-to-b from-[#BCF5CC] to-[#86B282]">
+          <h2 className="font-semibold text-2xl mb-3 mt-1 text-center">{`¡Hola ${user.firstName}!`}</h2>
           <div className="ml-4 mt-4 text-black font-semibold">
-          Historial contrataciones
-          </div>       
-         <ClientHistorialCard/>
+            Historial contrataciones
+          </div>
 
-     
-          
-
-         
-         
+          {historialData.adquired_services &&
+            historialData.adquired_services.length > 0 &&
+            historialData.adquired_services.map((service, index) => (
+              <ClientHistorialCard
+                key={index}
+                proveedoresContratado={service}
+              />
+            ))}
         </div>
       </div>
-      <FooterContact/>
+      <FooterContact />
     </>
   );
 };
