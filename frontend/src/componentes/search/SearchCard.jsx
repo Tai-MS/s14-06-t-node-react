92630f860e6aff3068d21f99f2ec582e240e91a0
Yein E <yeinneg@gmail.com>
2024-04-24 23:04:08 -0300

    conexionCardServicios

--- frontend/src/componentes/search/SearchCard.jsx
+++ frontend/src/componentes/search/SearchCard.jsx
@@ -1,26 +1,53 @@
 import { LoginButton } from "../share/LoginButtom";
 
+const imageUrls = [
+  "/images/userPhoto.svg",
+  "/images/userPhoto2.svg",
+  "/images/userPhoto3.svg",
+];
+
+let currentImageIndex = 0;
+
+const getNextImageUrl = () => {
+  const imageUrl = imageUrls[currentImageIndex];
+
+  currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+
+  return imageUrl;
+};
+
 export const SearchCard = ({ servicio }) => {
-  const { firstName, lastName, category, address, phone } = servicio;
+  console.log(servicio)
+  const imageUrl = getNextImageUrl();
 
   return (
     <div className="py-2 flex justify-center items-center">
       <div className="w-[328px] h-56 bg-green-200 bg-opacity-50 rounded-xl shadow-lg border border-green-200 flex items-center justify-center">
         <div>
-          <img
-            className="w-23 h-23 rounded-full ml-1"
-            src="/images/userPhoto.svg"
-            alt="User Photo"
-          />
+          <div className="flex justify-center items-center">
+            <img
+              className="w-23 h-23 rounded-full ml-1"
+              src={imageUrl} 
+              alt="User Photo"
+            />
+          </div>
           <div className="flex flex-col ml-1">
-            <span className="font-bold mb-0">{firstName} {lastName}</span>
+            <span className="font-bold mb-0 text-center">
+              {servicio.category}
+            </span>
             <div className="flex flex-col">
-              <label htmlFor="tarifa">Tarifa por hora:</label>
-              <span id="barrioNombre">$3.000</span>
+              <label htmlFor="tarifa" className="font-bold">
+                Tarifa por hora:
+              </label>
+              <span id="barrioNombre" className="text-center">
+                Por acordar
+              </span>
             </div>
           </div>
           <div className="w-[100%] flex">
-            <LoginButton to="*" width="w-full">Contratar</LoginButton>
+            <LoginButton to="*" width="w-full">
+              Contratar
+            </LoginButton>
           </div>
         </div>
         <div className="ml-10">
@@ -28,7 +55,7 @@ export const SearchCard = ({ servicio }) => {
             <label htmlFor="barrio" className="font-bold">
               Barrio:
             </label>
-            <span id="barrioNombre">{address}</span>
+            <span id="barrioNombre">Caba</span>
           </div>
           <div className="flex flex-col mt-2">
             <label htmlFor="disponibilidad" className="font-bold mb-0">
@@ -39,10 +66,9 @@ export const SearchCard = ({ servicio }) => {
           </div>
           <div className="flex flex-col mt-2">
             <label htmlFor="Calificacion" className="font-bold">
-              Calificación:
+              Servicio:
             </label>
-            <span id="calificacionNum">4.2</span>
-            <span id="verMas" className="underline">Ver más</span>
+            <span id="calificacionNum">{servicio.title}</span>
           </div>
         </div>
       </div>
